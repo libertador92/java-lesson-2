cb7209f9ef3ea3cd9051839aa487e8693ae2c573
libertador92 <miralizadenicat@gmail.com>
2024-06-14 07:07:17 -0400

    java 2

---
+++ app.js
@@ -0,0 +1,32 @@
+let name = 'Nijat';
+let surname = 'Miralizade';
+let fullName = name + ' ' + surname ;
+console.log(fullName);
+console.log(fullName[0]);
+console.log(fullName[6]);
+
+let ad = 'Robert';
+let soyad = 'Robespyer';
+let adSoyad = soyad + ' ' + ad ;
+console.log(adSoyad);
+
+console.log(`Salam ${fullName} necesen ? Zehmet olmasa mene ${adSoyad} haqqinda melumat vere bilersen ?`)
+
+
+
+ 
+// let text = "Nijat ist genius";
+// console.log(text);
+// console.log(text [4])
+// console.log(text.charAt(7));
+// console.log(text.indexOf('N'));
+// console.log(text.lastIndexOf('u'));
+// console.log(text.replace('N' , 'E'));
+// console.log(text.replaceAll('i','w'));
+// console.log(text.substring(0,5));
+// console.log(text.slice(0,5));
+// console.log(text.substr(5,9));
+// console.log(text.startsWith('x'));
+// console.log(text.startsWith('N'));
+// console.log(text.endsWith('s'));
+// console.log(text.endsWith('t'));
